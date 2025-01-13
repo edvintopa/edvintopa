f4e5de4ce396199331eb02eb8589fd04101d0221
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-01-13 10:01:37 +0100

    added page

---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <header className="landing-header">
+        <h1>Welcome to My Portfolio</h1>
+        <p>This is the landing page of my personal portfolio.</p>
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
