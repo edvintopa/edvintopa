6de8ebe0b44af5312bdaeae7b61c1430980b9a95
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-04-10 11:20:16 +0200

    Created projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Projects from './pages/Projects/Projects'
 import Layout from './components/Layout'
 import './App.css'
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </Layout>
     </Router>
---
+++ src/pages/Projects/Projects.tsx
@@ -0,0 +1,10 @@
+function Projects() {
+    return (
+      <div>
+        <h1>Projects Page</h1>
+        <p>This is a projects page.</p>
+      </div>
+    )
+  }
+  
+  export default Projects
