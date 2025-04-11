3d48c9e104dc77883a1127d6711d258589a33204
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-04-11 23:29:44 +0200

    updated tech stack

--- src/components/Containers/TechStack/TechStack.tsx
+++ src/components/Containers/TechStack/TechStack.tsx
@@ -3,7 +3,7 @@ import techStack from '../../../assets/data/techstack.json'
 
 const TechStack: React.FC = () => {
     return (
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-4">
             {techStack.map((tech, index) => (
                 <TechCard key={index} icon={tech.icon} title={tech.title} description={tech.description} />
             ))}
